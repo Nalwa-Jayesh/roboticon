b0429c4a60d5065cd981d74ffd3d7a7a5b565934
Jayesh Nalwa <nalwajayesh97@gmail.com>
2023-10-10 23:54:09 +0530

    ROS connect function

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+class AppConstants{
+  AppConstants._();
+
+  static const String SERVER_API_URL = "http://192.168.1.8:8000/";
+  static const String _vidBase = "assets/vids";
+  static const String defaultVid = "$_vidBase/default.mp4";
+  static const String boredVid = "$_vidBase/bored.mp4";
+  static const String angryVid = "$_vidBase/angry.mp4";
+}
